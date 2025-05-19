4e4688e2d5f066bcdefb207cd67f3653c76cd8a9
Your Name <you@example.com>
2025-05-19 20:50:17 +0700

    Add Slot 1 folder with exercise-1 project

---
+++ Slot 1/myfirstreact/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+
+function App() {
+  const h1Style = {
+    color: "blue",
+    textAlign: "center",
+  };
+  return (
+    <div>
+      <h1 style={h1Style}>Hello world!</h1>
+    </div>
+  );
+}
+
+
+export default App;
